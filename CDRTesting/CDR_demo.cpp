5879afcf9035f3e291601c2700f9c17ad04da7d5
RichardMartinez <rmartinez21@vt.edu>
2024-12-02 22:12:19 -0500

    working char bank

--- CDRTesting/CDR_demo.cpp
+++ CDRTesting/CDR_demo.cpp
@@ -4,8 +4,10 @@
 #include <bitset>
 #include <cstdint>
 #include <sstream>
+#include <vector>
 
 typedef std::unordered_map<std::string, std::string> WordBank;
+typedef WordBank CharBank;
 
 // Construct word bank from dictionary file
 // Key = common word
@@ -34,6 +36,49 @@ WordBank construct_word_bank(std::string filename) {
     return word_bank;
 }
 
+CharBank construct_char_bank() {
+    CharBank char_bank;
+
+    std::vector<std::string> char_list;
+
+    // Add characters 'a' to 'z' to the vector
+    for (char c = 'a'; c <= 'z'; ++c) {
+        char_list.push_back(std::string(1, c));
+    }
+
+    // Add digits '0' to '9' to the vector
+    for (char d = '0'; d <= '9'; ++d) {
+        char_list.push_back(std::string(1, d));
+    }
+
+    // Add special characters to the vector
+    std::string special_chars = ".,?!-=+/::;()'\"~@#$%&*";
+    for (char sc : special_chars) {
+        char_list.push_back(std::string(1, sc));
+    }
+
+    // Add digraphs to the vector
+    std::vector<std::string> digraphs = {"sh", "ch", "th", "ck", "ie", "ea", "ee"};
+    for (const auto& digraph : digraphs) {
+        char_list.push_back(digraph);
+    }
+
+
+    uint16_t code_point = 0;
+    for (auto x : char_list) {
+        std::bitset<6> bitset(code_point);
+
+        std::ostringstream oss;
+        oss << bitset;
+        std::string binary_code = oss.str();
+
+        char_bank[x] = binary_code;
+        code_point++;
+    }
+
+    return char_bank;
+}
+
 
 int main() {
 
@@ -44,11 +89,13 @@ int main() {
     filename = "dict.txt";
     WordBank word_bank = construct_word_bank(filename);
 
-    for (auto word_code_pair : word_bank) {
-        std::string word = word_code_pair.first;
-        std::string code = word_code_pair.second;
+    CharBank char_bank = construct_char_bank();
 
-        std::cout << word << " : " << code << std::endl;
+    for (auto char_code_pair : char_bank) {
+        std::string char_str = char_code_pair.first;
+        std::string binary_code = char_code_pair.second;
+
+        std::cout << char_str << " : " << binary_code << std::endl;
     }
 
 }

51be5ea000da40211f8e45accd19236347915f6e
JacobTheComputerEngineer <jacobramirez07@vt.edu>
2025-01-23 13:22:13 -0500

    progress on prepro

--- ERL_encoder/main.cpp
+++ ERL_encoder/main.cpp
@@ -6,11 +6,17 @@
 
 class ERL
 {
-parser_ par;
-preprocessor_ pre;
-encoder_ enc;
-decoder_ dec;
-wordBank_ wb;
+    public:
+        parser_ par;
+        preprocessor_ pre;
+        encoder_ enc;
+        decoder_ dec;
+        wordBank_ wb;
+
+        //functions connecting modules here
+        //module specific functions in each module
+    private:
+
 };
 
 int main(int argc, char *argv[])
--- ERL_encoder/preprocessor.h
+++ ERL_encoder/preprocessor.h
@@ -1,4 +1,13 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
 class preprocessor_
 {
-
+    public:
+        std::ifstream iFile;
+        std::string inputWord;
+        std::vector<std::string> outputWords;
+    private:
+        
 };

e07580daa3edaead178f45408447d1425bb8eca3
JacobTheComputerEngineer <jacobramirez07@vt.edu>
2025-01-23 13:02:34 -0500

    Basic system added

---
+++ ERL_encoder/main.cpp
@@ -0,0 +1,10 @@
+#include "decoder.h"
+#include "encoder.h"
+#include "parser.h"
+#include "preprocessor.h"
+#include "wordBank.h"
+
+int main(int argc, char *argv[])
+{
+
+}
---
+++ ERL_encoder/preprocessor.h
@@ -0,0 +1,4 @@
+class preprocessor
+{
+
+};

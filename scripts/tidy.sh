d1fb79ed435e149c2b5ee62aacd333aea4221059
RichardMartinez <rmartinez21@vt.edu>
2025-01-31 22:25:03 -0500

    reformat tidy.sh

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -4,12 +4,11 @@
 for f in $PWD/../*.cpp
 do
     # Use clang-tidy with color enabled and filter relevant messages
+    echo "Running clang-tidy on $f"
     clang-tidy -p=$PWD -header-filter=$dir $f -config="{Checks: '-*,readability-*,-readability-magic-numbers', CheckOptions: [{key: readability-function-size.LineThreshold, value: 200}]}" 2>&1 | \
-    awk '
-        /warning:|error:|note:/ { keep = 3 }  # Start keeping when a warning, error, or note is found
-        keep-- >= 0 { print }                 # Print the match and the next 3 lines
-    ' | sed -e "s/\(warning:\)/\x1b[33m\1\x1b[0m/g" \
-            -e "s/\(error:\)/\x1b[31m\1\x1b[0m/g" \
-            -e "s/\(note:\)/\x1b[36m\1\x1b[0m/g" \
-            -e "s/\([~^]\)/\x1b[32m\1\x1b[0m/g"
+    awk '/warning:|error:|note:/ { keep = 3 } keep-- >= 0 { print }' | \
+    sed -e "s/\(warning:\)/\x1b[33m\1\x1b[0m/g" \
+        -e "s/\(error:\)/\x1b[31m\1\x1b[0m/g" \
+        -e "s/\(note:\)/\x1b[36m\1\x1b[0m/g" \
+        -e "s/\([~^]\)/\x1b[32m\1\x1b[0m/g"
 done

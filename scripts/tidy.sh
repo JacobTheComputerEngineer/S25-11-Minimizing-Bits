bff1ef3c1f9c271c4d9091cb58dddd40f1ff0c34
RichardMartinez <rmartinez21@vt.edu>
2025-01-31 22:12:52 -0500

    simplify for loop in tidy.sh

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,10 +1,7 @@
 
 #!/bin/sh
 # A little bit of a hack, make sure you use the cmake target in the build directory
-dir=$PWD
-allfiles=$(ls $dir/../*.cpp)
-
-for f in $allfiles
+for f in $PWD/../*.cpp
 do
     # Use clang-tidy with color enabled and filter relevant messages
     clang-tidy -p=$PWD -header-filter=$dir $f -config="{Checks: '-*,readability-*,-readability-magic-numbers', CheckOptions: [{key: readability-function-size.LineThreshold, value: 200}]}" 2>&1 | \

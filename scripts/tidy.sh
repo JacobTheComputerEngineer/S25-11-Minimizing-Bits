4c6bdb72f9c0593816e0f45689072fdfe53540ff
RichardMartinez <rmartinez21@vt.edu>
2025-02-01 16:51:43 -0500

    remove extra tidy.sh commands

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
+# A little bit of a hack but it works, make sure to use the cmake target from the build directory
 for f in $PWD/../*.cpp
 do
     echo "-- Running clang-tidy on $f"
-    clang-tidy -quiet -p=$PWD -header-filter=$dir $f -config="{Checks: '-*,readability-*,-readability-magic-numbers', CheckOptions: [{key: readability-function-size.LineThreshold, value: 200}]}" 2>&1 | \
-    awk '!/warnings generated/' | \
-    sed -e "s/\(warning:\)/\x1b[33m\1\x1b[0m/g" \
-        -e "s/\(error:\)/\x1b[31m\1\x1b[0m/g" \
-        -e "s/\(note:\)/\x1b[36m\1\x1b[0m/g" \
-        -e "s/\([~^]\)/\x1b[32m\1\x1b[0m/g"
+    clang-tidy -quiet -p=$PWD -header-filter=$dir $f -config="{Checks: '-*,readability-*,-readability-magic-numbers', CheckOptions: [{key: readability-function-size.LineThreshold, value: 200}]}"
 done

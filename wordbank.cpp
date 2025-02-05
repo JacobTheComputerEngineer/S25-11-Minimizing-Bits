3cbfdcd136c2d6cc84595e9ca8d2240f90dd5560
RichardMartinez <rmartinez21@vt.edu>
2025-02-04 22:39:22 -0500

    stub out wordbank

--- wordbank.cpp
+++ wordbank.cpp
@@ -1,1 +1,43 @@
 #include "wordbank.h"
+
+wordbank_::wordbank_(): wordbank_("dictionary.txt") {}
+
+wordbank_::wordbank_(const std::string& filename) {
+
+}
+
+bit_code_13_ wordbank_::word_to_code(const std::string& word) const {
+    return bit_code_13_(0);
+}
+
+std::string wordbank_::code_to_word(bit_code_13_ code) const {
+    return std::string();
+}
+
+bit_code_6_ wordbank_::char_to_code(const std::string& c) const {
+    return bit_code_6_();
+}
+
+std::string wordbank_::code_to_char(bit_code_6_ code) const {
+    return std::string();
+}
+
+bool wordbank_::contains_word(const std::string& word) const {
+    return false;
+}
+
+std::string wordbank_::getExistingChars() {
+    return std::string();
+}
+
+std::string wordbank_::getPunctuationChars() {
+    return std::string();
+}
+
+void wordbank_::build_word_map(const std::string& filename) {
+
+}
+
+void wordbank_::build_char_map() {
+
+}
--- wordbank.h
+++ wordbank.h
@@ -8,5 +8,36 @@ typedef std::bitset<6> bit_code_6_;
 
 class wordbank_
 {
+    public:
+
+        // Default constructor
+        wordbank_();
+
+        // Construct the word bank from a given dictionary file
+        wordbank_(const std::string& filename);
+
+        // Access word bank
+        bit_code_13_ word_to_code(const std::string& word) const;
+        std::string code_to_word(bit_code_13_ code) const;
+
+        // Access char bank
+        bit_code_6_ char_to_code(const std::string& c) const;
+        std::string code_to_char(bit_code_6_ code) const;
+
+        // Helper functions
+        bool contains_word(const std::string& word) const;
+        static std::string getExistingChars();
+        static std::string getPunctuationChars();
+
+    private:
+
+        std::unordered_map<std::string, bit_code_13_> forward_word_map;
+        std::unordered_map<bit_code_13_, std::string> reverse_word_map;
+
+        std::unordered_map<std::string, bit_code_6_> forward_char_map;
+        std::unordered_map<bit_code_6_, std::string> reverse_char_map;
+
+        void build_word_map(const std::string& filename);
+        void build_char_map();
 
 };

b8916d1e543a165f4f465e9a12b7f2cba55d1b5d
Kylan Montgomery <kylanm24@vt.edu>
2025-02-21 13:17:09 -0500

    Added test cases for parser

--- test_parser.cpp
+++ test_parser.cpp
@@ -2,10 +2,39 @@
 
 #include "parser.h"
 
-TEST_CASE("example parser test", "[parser]") {
+#include <fstream>
+
+TEST_CASE("Test file validation and if empty", "[parser]") {
     parser_ par;
 
-    REQUIRE(true);
+    // Example test case
+    std::string testfile = "testparser.txt";
+    //check if testparser is a valid file
+    REQUIRE(par.isValidFile(testfile) == true);
+    //check if testparser is empty
+    REQUIRE(par.isFileEmpty(testfile) == false);
+
 }
 
-// TODO: Add more tests
+TEST_CASE("Test file creation", "[parser]") {
+    parser_ par;
+    std::string testfile = "testoutput.txt";
+    //create a file
+    par.createOutputFile(testfile);
+    //check if the file exists
+    std::ifstream infile(testfile);
+    REQUIRE(infile.good() == true);
+    infile.close();
+}
+
+TEST_CASE("Test argument parsing", "[parser]") {
+    parser_ par;
+    std::string messageFile = "testparser.txt";
+    std::string dictionaryFile = "dictionary.txt";
+    //check if the arguments are parsed correctly
+    char* validArgs[] = { (char*)"program", (char*)"testparser.txt", (char*)"dictionary.txt" };
+    REQUIRE(par.parseArguments(3, validArgs, messageFile, dictionaryFile) == true);
+    REQUIRE(messageFile == "testparser.txt");
+    REQUIRE(dictionaryFile == "dictionary.txt");
+}
+
